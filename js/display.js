253d355a0c35fd57c318e768a70045a8bec9a5ed
CLOTILDE FAUCHILLE <clotildefauchille@macbook-pro-de-clotilde.home>
2023-03-20 13:47:34 +0100

    delete some consoleLog

--- js/display.js
+++ js/display.js
@@ -1,24 +1,6 @@
 
-function toggleText1() {
-    var span = document.getElementById("paraph-hidden");
-    if (span.style.display == "none") {
-        span.style.display = "inline";
-    } else {
-        span.style.display = "none";
-    }
-}
-
-function toggleText2() {
-    var span = document.getElementById("paraph-hidden-2");
-    if (span.style.display == "none") {
-        span.style.display = "inline";
-    } else {
-        span.style.display = "none";
-    }
-}
-
-function toggleText3    () {
-    var span = document.getElementById("paraph-hidden-3");
+function toggleText(id) {
+    var span = document.getElementById(id);
     if (span.style.display == "none") {
         span.style.display = "inline";
     } else {
