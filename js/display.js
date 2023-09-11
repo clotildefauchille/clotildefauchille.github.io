cbaf61737e2b3308ea3ec60d9c7a8be21de95a71
CLOTILDE FAUCHILLE <clotildefauchille@macbook-pro-de-clotilde.home>
2023-09-11 23:46:53 +0200

    update

--- js/display.js
+++ js/display.js
@@ -7,3 +7,17 @@ function toggleText(id) {
         span.style.display = "none";
     }
 }
+
+document.querySelectorAll('.nav-link').forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1); 
+        const targetElement = document.getElementById(targetId); 
+
+        if (targetElement) {
+            const yOffset = -70; 
+            const y = targetElement.getBoundingClientRect().top + window.pageYOffset + yOffset;
+            window.scrollTo({ top: y, behavior: 'smooth' });
+        }
+    });
+});
